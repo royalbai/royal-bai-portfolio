48c61e93903d32a9979addb1121f7b79de979183
Royal <royalbai9@gmail.com>
2022-05-10 22:18:58 -0400

    adjusted some project text and fixed the syntax error in scripts

--- scripts.js
+++ scripts.js
@@ -1,28 +1,27 @@
 const menuIcon = document.querySelector(".burgerIcon");
 const menuSlideout = document.querySelector("nav");
-
-menuIcon.addEventListener("click", function () {
-    menuIcon.classList.toggle("active");
-    menuSlideout.classList.toggle("activeNav");
-});
-
-const links = document.querySelectorAll("nav a")
-// console.log(links);
-
-links.forEach(link => {
-    // console.log(link);
-    link.addEventListener("click", function () {
-        // console.log("Clicked");
-        menuSlideout.classList.remove("activeNav");
-        menuIcon.classList.remove("active");
-    })
-})
-
+const links = document.querySelectorAll("nav a");
 const card = document.querySelectorAll(".innerCard");
 
-card.forEach(item => {
-    item.addEventListener("click", (e) => {
-        const cc = e.target.parentElement;
-        cc.classList.toggle("flip");
+const handle = () => {
+    menuIcon.addEventListener("click", function () {
+        menuIcon.classList.toggle("active");
+        menuSlideout.classList.toggle("activeNav");
+    });
+
+    links.forEach(link => {
+        link.addEventListener("click", function () {
+            menuSlideout.classList.remove("activeNav");
+            menuIcon.classList.remove("active");
+        })
     })
-})
+
+    card.forEach(item => {
+        item.addEventListener("click", (e) => {
+            const cc = e.target.parentElement;
+            cc.classList.toggle("flip");
+        })
+    })
+}
+
+handle();
